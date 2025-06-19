52d11d3db270cdaa7fc083302f3d5321d7caa081
VoidArc Studio <chudybyczek2137@gmail.com>
2025-06-19 13:58:03 +0200

    Update unpack.sh

--- unpack.sh
+++ unpack.sh
@@ -5,17 +5,17 @@ chmod a+x "$0"
 
 # Sprawdź, czy katalog źródłowy istnieje
 if [ ! -d "/tmp/HackerOS-Updates/HackerOS/" ]; then
-    echo "Błąd: Katalog /tmp/HackerOS-Updates/HackerOS/ nie istnieje."
+    echo "Error: Directory /tmp/HackerOS-Updates/HackerOS/ does not exist."
     exit 1
 fi
 
 # Usuń istniejący katalog /usr/share/HackerOS/
-echo "Usuwam /usr/share/HackerOS/..."
+echo "Deleting /usr/share/HackerOS/..."
 sudo rm -rf /usr/share/HackerOS/
 
 # Przenieś katalog do /usr/share/
-echo "Przenoszę nowy katalog HackerOS do /usr/share/..."
+echo "Moving new HackerOS directory to /usr/share/..."
 sudo mv /tmp/HackerOS-Updates/HackerOS/ /usr/share/
-chmod a+x /usr/share/HackerOS/Scripts/Bin/hacker_mode.sh
+chmod a+x /usr/share/HackerOS/Scripts/Bin/hacker_mode.sh /usr/share/HackerOS/Scripts/Bin/Hacker-Mode-Update.sh /usr/share/HackerOS/Scripts/Bin/Hacker-Unpack.sh /usr/share/HackerOS/Scripts/Bin/Hacker-Update.sh /usr/share/HackerOS/Scripts/Bin/HackerOS-Documentation.sh /usr/share/HackerOS/Scripts/Bin/HackerOS-TV.sh /usr/share/HackerOS/Scripts/Bin/install-penetration-tools.sh /usr/share/HackerOS/Scripts/Bin/install-tools.sh /usr/share/HackerOS/Scripts/Bin/revert_to_plasma.sh /usr/share/HackerOS/Scripts/Bin/Switch_To_Other_Session.sh /usr/share/HackerOS/Scripts/Bin/update_system.sh 
 
-echo "Operacja zakończona sukcesem."
+echo "The operation was completed successfully."
