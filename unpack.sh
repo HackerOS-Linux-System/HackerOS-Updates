dc901714f10b279f946d6035ab2e186de9ee1c26
VoidArc Studio <chudybyczek2137@gmail.com>
2025-06-19 07:09:48 -0400

    Create unpack.sh

---
+++ unpack.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Daj uprawnienia do wykonywania dla samego siebie
+chmod a+x "$0"
+
+# Sprawdź, czy katalog źródłowy istnieje
+if [ ! -d "/tmp/HackerOS-Updates/HackerOS/" ]; then
+    echo "Błąd: Katalog /tmp/HackerOS-Updates/HackerOS/ nie istnieje."
+    exit 1
+fi
+
+# Usuń istniejący katalog /usr/share/HackerOS/
+echo "Usuwam /usr/share/HackerOS/..."
+sudo rm -rf /usr/share/HackerOS/
+
+# Przenieś katalog do /usr/share/
+echo "Przenoszę nowy katalog HackerOS do /usr/share/..."
+sudo mv /tmp/HackerOS-Updates/HackerOS/ /usr/share/
+chmod a+x /usr/share/HackerOS/Scripts/Bin/hacker_mode.sh
+
+echo "Operacja zakończona sukcesem."
