89e68f5afc1d5bb03b312e5955c34499c96dd515
VoidArc Studio <chudybyczek2137@gmail.com>
2025-06-20 13:45:10 +0200

    Update check_updates_notify.sh

--- HackerOS/Scripts/Bin/check_updates_notify.sh
+++ HackerOS/Scripts/Bin/check_updates_notify.sh
@@ -13,14 +13,41 @@ SNAP_UPDATES=$(snap refresh --list | grep -c "^")
 TOTAL_UPDATES=$((APT_UPDATES + FLATPAK_UPDATES + SNAP_UPDATES))
 
 if [ "$TOTAL_UPDATES" -gt 0 ]; then
-    # Wyswietl GUI z Zenity
-    zenity --info \
-        --title="Dostępne aktualizacje systemowe" \
-        --width=350 \
-        --height=150 \
-        --ok-label="Zaktualizuj system" \
-        --text="Wykryto aktualizacje:\n• APT: $APT_UPDATES\n• Flatpak: $FLATPAK_UPDATES\n• Snap: $SNAP_UPDATES\n\nKliknij przycisk, aby rozpocząć aktualizację."
+    # Parametry okna
+    WIDTH=350
+    HEIGHT=200
 
-    # Po kliknięciu przycisku uruchom skrypt aktualizacji
-    bash /usr/share/HackerOS/Scripts/Bin/update_system.sh
+    # Pobierz rozmiar ekranu
+    SCREEN_WIDTH=$(xrandr | grep '*' | awk '{print $1}' | cut -d'x' -f1)
+    SCREEN_HEIGHT=$(xrandr | grep '*' | awk '{print $1}' | cut -d'x' -f2)
+
+    # Pozycja prawy dolny róg (20 px od krawędzi)
+    POS_X=$((SCREEN_WIDTH - WIDTH - 20))
+    POS_Y=$((SCREEN_HEIGHT - HEIGHT - 60))
+
+    # Uruchom zenity w tle
+    (
+        zenity --question \
+            --title="Dostępne aktualizacje systemowe" \
+            --width=$WIDTH \
+            --height=$HEIGHT \
+            --text="Wykryto aktualizacje:\n• APT: $APT_UPDATES\n• Flatpak: $FLATPAK_UPDATES\n• Snap: $SNAP_UPDATES\n\nCzy chcesz zaktualizować system?"
+        echo $? > /tmp/zenity_response
+    ) &
+
+    # Poczekaj aż okno się pojawi
+    sleep 0.5
+
+    # Przesuń okno na prawy dolny róg
+    wmctrl -r "Dostępne aktualizacje systemowe" -e 0,$POS_X,$POS_Y,$WIDTH,$HEIGHT
+
+    # Poczekaj na zakończenie zenity
+    wait
+
+    RESPONSE=$(cat /tmp/zenity_response)
+    rm /tmp/zenity_response
+
+    if [ "$RESPONSE" -eq 0 ]; then
+        bash /usr/share/HackerOS/Scripts/Bin/update_system.sh
+    fi
 fi
