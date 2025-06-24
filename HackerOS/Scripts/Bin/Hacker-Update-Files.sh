1ab1881d2b4853ebdda742a860b1e4a69e2286ec
VoidArc Studio <chudybyczek2137@gmail.com>
2025-06-24 18:07:51 +0200

    Create Hacker-Update-Files.sh

---
+++ HackerOS/Scripts/Bin/Hacker-Update-Files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Skrypt do kopiowania plików konfiguracyjnych i ikon w HackerOS
+
+echo "Kopiowanie plików .desktop..."
+sudo cp -r /usr/share/HackerOS/Config-Files/steam.desktop /usr/share/applications/
+sudo cp -r /usr/share/HackerOS/Config-Files/org.gnome.Software.desktop /usr/share/applications/
+
+echo "Kopiowanie ikon Plymouth..."
+sudo cp -r /usr/share/HackerOS/ICONS/Plymouth-Icons/bgrt-fallback.png /usr/share/plymouth/themes/spinner/
+sudo cp -r /usr/share/HackerOS/ICONS/Plymouth-Icons/ubuntu-logo.png /usr/share/plymouth/
+sudo cp -r /usr/share/HackerOS/ICONS/Plymouth-Icons/watermark.png /usr/share/plymouth/themes/spinner/
+
+echo "Kopiowanie zakończone!"
