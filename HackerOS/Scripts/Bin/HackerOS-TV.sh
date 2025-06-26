d6b839f1df58dcc3c717172217efd22fb7cfd034
Michal92299 <michal.99967@gmail.com>
2025-06-26 19:36:36 +0200

    Update HackerOS-TV.sh

--- HackerOS/Scripts/Bin/HackerOS-TV.sh
+++ HackerOS/Scripts/Bin/HackerOS-TV.sh
@@ -3,11 +3,5 @@
 # Przejdź do katalogu aplikacji
 cd /usr/share/HackerOS/Scripts/HackerOS-Apps/HackerOS-TV/
 
-# Aktywuj wirtualne środowisko
-source venv/bin/activate
-
-# Uruchom aplikację Python
-python app.py
-
-# (Opcjonalnie) dezaktywuj środowisko po zakończeniu
-deactivate
+#Uruchamiamy
+npm start
