83f62a1605d79135373c2e65b4e88cd3c9ba85e2
Michal92299 <michal.99967@gmail.com>
2025-07-03 14:16:05 +0200

    Update unpack.sh

--- unpack.sh
+++ unpack.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Sudo Mode
+sudo su
+
 # Daj uprawnienia do wykonywania dla samego siebie
 chmod a+x "$0"
 
@@ -11,19 +14,22 @@ fi
 
 # Usuń istniejący katalog /usr/share/HackerOS/
 echo "Deleting /usr/share/HackerOS/..."
-sudo rm -rf /usr/share/HackerOS/
+rm -rf /usr/share/HackerOS/
 
 # Przenieś katalog do /usr/share/
 echo "Moving new HackerOS directory to /usr/share/..."
-sudo mv /tmp/HackerOS-Updates/HackerOS/ /usr/share/
+
+#HackerOS File Update
+mv /tmp/HackerOS-Updates/HackerOS/ /usr/share/
+
+#Permission Update
 chmod a+x /usr/share/HackerOS/Scripts/Bin/hacker_mode.sh /usr/share/HackerOS/Scripts/Bin/Hacker-Mode-Update.sh /usr/share/HackerOS/Scripts/Bin/Hacker-Unpack.sh /usr/share/HackerOS/Scripts/Bin/Hacker-Update.sh /usr/share/HackerOS/Scripts/Bin/HackerOS-Documentation.sh /usr/share/HackerOS/Scripts/Bin/HackerOS-TV.sh /usr/share/HackerOS/Scripts/Bin/install-penetration-tools.sh /usr/share/HackerOS/Scripts/Bin/install-tools.sh /usr/share/HackerOS/Scripts/Bin/revert_to_plasma.sh /usr/share/HackerOS/Scripts/Bin/Switch_To_Other_Session.sh /usr/share/HackerOS/Scripts/Bin/update_system.sh  /usr/share/HackerOS/Scripts/Bin/Proton-Updater.sh /usr/share/HackerOS/Scripts/Bin/Hacker-Update-Files.sh /usr/share/HackerOS/Scripts/Steam/HackerOS-Steam.sh /usr/share/HackerOS/Scripts/Steam/HackerOS-Steam-Animation.sh /usr/share/HackerOS/Scripts/Steam/HackerOS-Steam-For-Hacker-Mode.sh /usr/share/HackerOS/Scripts/Bin/update.sh /usr/share/HackerOS/Scripts/Bin/hacker-install-gamescope-steam.sh
 
 echo "Updating bash"
-sudo mv /usr/share/HackerOS/Config-Files/bash.bashrc /etc/
+mv /usr/share/HackerOS/Config-Files/bash.bashrc /etc/
 
 echo "Updating Hacker Mode and HackerOS TV"
-#Admin Mode
-sudo su
+
 #Updating HackerOS TV
 cd /usr/share/HackerOS/Scripts/HackerOS-Apps/HackerOS-TV/
 npm insttall
