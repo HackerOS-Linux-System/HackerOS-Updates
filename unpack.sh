a2057338a0b59f809176ec2a2eb05992e39876a1
VoidArc Studio <chudybyczek2137@gmail.com>
2025-08-14 19:53:03 +0200

    Update unpack.sh

--- unpack.sh
+++ unpack.sh
@@ -44,6 +44,7 @@ chmod a+x /usr/share/HackerOS/Scripts/Bin/* \
 #Update HackerOS apps
 cd /usr/share/HackerOS/Scripts/HackerOS-Apps/HackerOS-TV/
 python3 -m venv HackerOS-TV
+source /usr/share/HackerOS/Scripts/HackerOS-Apps/HackerOS-TV/HackerOS-TV/bin/activate
 pip install pyqt6
 
 cd /usr/share/HackerOS/Scripts/HackerOS-Apps/Hacker-Mode/ && npm install
