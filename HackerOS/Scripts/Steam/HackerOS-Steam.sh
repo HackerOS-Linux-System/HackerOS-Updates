0d8e8f1e8ad423c715f52454d202840cf4eff824
michal92299 <chudybyczek2137@gmail.com>
2026-01-02 18:33:19 +0100

    Update HackerOS-Steam.sh

--- HackerOS/Scripts/Steam/HackerOS-Steam.sh
+++ HackerOS/Scripts/Steam/HackerOS-Steam.sh
@@ -6,7 +6,7 @@ ANIMATION_SCRIPT="/usr/share/HackerOS/Scripts/Steam/HackerOS-Steam-Animation.sh"
 # Instalacja Steam jeśli potrzeba
 if ! command -v steam &> /dev/null; then
     zenity --info --text="Steam nie jest zainstalowany. Instaluję..."
-    sudo apt update && sudo apt install -y steam
+    distrobox create HackerOS-Steam --image archlinux:latest && distrobox enter HackerOS-Steam -- echo -e '\n[multilib]\nInclude = /etc/pacman.d/mirrorlist' | sudo tee -a /etc/pacman.conf >/dev/null && distrobox enter HackerOS-Steam -- sudo pacman -Syu --noconfirm && distrobox enter HackerOS-Steam -- sudo pacman -S --noconfirm steam lib32-mesa lib32-vulkan-icd-loader lib32-alsa-lib lib32-gcc-libs lib32-gtk3 lib32-libgcrypt lib32-libpulse lib32-libva lib32-libxml2 lib32-nss lib32-openal lib32-sdl2 lib32-vulkan-intel lib32-vulkan-radeon lib32-nvidia-utils lib32-libxss lib32-libgpg-error lib32-dbus gnu-free-fonts noto-fonts ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid ttf-ibm-plex ttf-input ttf-input-nerd ttf-liberation ttf-roboto lib32-vulkan-asahi lib32-vulkan-dzn lib32-vulkan-freedreno lib32-vulkan-gfxstream lib32-vulkan-nouveau lib32-vulkan-swrast lib32-vulkan-virtio
 fi
 
 # GUI wyboru trybu uruchomienia
