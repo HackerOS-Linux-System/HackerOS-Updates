09abb60e227aecebc340285e317a6dd85920cd42
michal92299 <chudybyczek2137@gmail.com>
2026-02-23 11:03:35 +0100

    Update unpack.sh

--- unpack.sh
+++ unpack.sh
@@ -50,7 +50,7 @@ cd /usr/bin/ || exit
 sudo rm -f hpm hacker ngt hedit a HackerOS-Steam hbuild ghdir
 
 sudo curl -L -o hedit "https://github.com/HackerOS-Linux-System/hedit/releases/download/v0.3/hedit"
-sudo curl -L -o ngt "https://github.com/HackerOS-Linux-System/ngt/releases/download/v0.2/ngt"
+sudo curl -L -o ngt "https://github.com/HackerOS-Linux-System/ngt/releases/download/v0.3/ngt"
 sudo curl -L -o hacker "https://github.com/HackerOS-Linux-System/Hacker-CLI-Tool/releases/download/v2.2/hacker"
 sudo curl -L -o hpm "https://github.com/HackerOS-Linux-System/Hacker-Package-Manager/releases/download/v0.5/hpm"
 sudo curl -L -o HackerOS-Steam "https://github.com/HackerOS-Linux-System/HackerOS-Steam/releases/download/v0.2/HackerOS-Steam"
